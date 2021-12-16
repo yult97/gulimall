9db887834c6af4035c0c3b002a920da797587dd0
EFL <1904282367@qq.com>
2021-12-16 23:07:24 +0800

    day1-2 commit master project

---
+++ gulimall-poduct/src/test/java/com/atguigu/gulimall/poduct/GulimallPoductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.poduct;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallPoductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
