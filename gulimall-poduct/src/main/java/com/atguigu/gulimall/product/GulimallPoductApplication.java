6fc338c2184c02b361e867a1540f9a5f2f3e3383
yult97 <1904282367@qq.com>
2023-01-30 01:00:54 +0800

    品牌测试类编写以及工具类服务描述增加

--- gulimall-poduct/src/main/java/com/atguigu/gulimall/product/GulimallPoductApplication.java
+++ gulimall-poduct/src/main/java/com/atguigu/gulimall/product/GulimallPoductApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1.整合 mybatis-plus
+ */
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallPoductApplication {
--- gulimall-poduct/src/test/java/com/atguigu/gulimall/product/GulimallPoductApplicationTests.java
+++ gulimall-poduct/src/test/java/com/atguigu/gulimall/product/GulimallPoductApplicationTests.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,25 +16,38 @@ import java.util.List;
 @SpringBootTest
 public class GulimallPoductApplicationTests {
 
+    /**
+     * 品牌实现类
+     */
     @Autowired
     BrandService brandService;
 
     @Test
     public void contextLoads() {
-//          插入操作
-//        BrandEntity brandEntity=new BrandEntity();
-//        brandEntity.setName("华为");
-//        brandService.save(brandEntity);
-//        System.out.println("...............保存成功");
-//          更新操作
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setBrandId(1l);
-//        brandEntity.setDescript("华为");
-//        brandService.updateById(brandEntity);
-//        System.out.println("...............更新成功");
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("手机");
+        brandEntity.setLogo("小米");
+        brandEntity.setDescript("小米手机4G");
+        brandEntity.setShowStatus(1);
+        brandEntity.setFirstLetter("小");
+        brandEntity.setSort(1);
+        boolean save = brandService.save(brandEntity);
+        System.out.println("========商品插入成功=======" + save);
 
-        List<BrandEntity> entityList = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1));
-        entityList.forEach((item)-> System.out.println(item));
+        //查询品牌信息数据
+        List<BrandEntity> entityList = brandService.list();
+        entityList.forEach((item) -> System.out.println(item));
+
+        //根据编号查询品牌信息数据
+        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_Id", 2));
+        brand_id.forEach((item) -> System.out.println(item));
+
+
+        //更新操作
+        brandEntity.setBrandId(1l);
+        brandEntity.setDescript("华为");
+        brandService.updateById(brandEntity);
+        System.out.println("...............更新成功");
     }
 
 }
