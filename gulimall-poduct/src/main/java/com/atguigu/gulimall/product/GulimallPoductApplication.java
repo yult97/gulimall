5e66524b7ad77623c9f20e3290860085a5b30711
yult97 <1904282367@qq.com>
2023-02-17 00:15:39 +0800

    商品服务商品分类树形显示

--- gulimall-poduct/src/main/java/com/atguigu/gulimall/product/GulimallPoductApplication.java
+++ gulimall-poduct/src/main/java/com/atguigu/gulimall/product/GulimallPoductApplication.java
@@ -3,11 +3,13 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1.整合 mybatis-plus
  */
 @MapperScan("com.atguigu.gulimall.product.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallPoductApplication {
 
